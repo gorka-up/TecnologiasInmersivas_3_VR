ff012d62409619e109871b72ee3686307f57385d
Gorka Urrutia del Pico <gorka.gamedev@gmail.com>
2026-03-23 10:11:19 +0100

    wip: create project

---
+++ Assets/GorkaUrrutia/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    GameManager instance;
+
+    public enum Difficulty
+    {
+        Easy,
+        Medium,
+        Hard
+    }
+    [Header("Difficulty")]
+    [SerializeField]
+    public Difficulty actualDifficulty;
+
+    public Canvas canvas;
+    public Button easyButton, mediumButton, hardButton;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        SetEasy();
+    }
+
+    void Update()
+    {
+        
+    }   
+
+    public void SetEasy()
+    {
+        actualDifficulty = Difficulty.Easy;
+        easyButton.colors.Equals(Color.green);
+        mediumButton.colors.Equals(Color.gray);
+        hardButton.colors.Equals(Color.gray);
+    }
+    public void SetMedium()
+    {
+        actualDifficulty = Difficulty.Medium;
+        easyButton.colors.Equals(Color.gray);
+        mediumButton.colors.Equals(Color.yellow);
+        hardButton.colors.Equals(Color.gray);
+    }
+    public void SetHard()
+    {
+        actualDifficulty = Difficulty.Hard;
+        easyButton.colors.Equals(Color.gray);
+        mediumButton.colors.Equals(Color.gray);
+        hardButton.colors.Equals(Color.red);
+    }
+}
